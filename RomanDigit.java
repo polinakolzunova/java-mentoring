b4790ec91f7f3b177b4a8fcaa6b4ad3b7f41f7ed
polinakolzunova <polinakolzunova@gmail.com>
2020-10-28 11:02:58 +0300

    improve convertToRoman method

--- RomanDigit.java
+++ RomanDigit.java
@@ -29,42 +29,69 @@ public class RomanDigit{
     }
 
     public static String convertToRoman(int number) {
-        char c[] = new char[10];
-        int i = 0;
+        String result = "";
+
+        if (number == 0) return "nulla";
+
+        if (number < 0) {
+            number *= -1;
+            result += "-";
+        }
+
         while (number != 0) {
-            if (number >= 10) {
+            if (number >= 100) {
+                if (number < 400) {
+                    result = digit('C', number / 100, result);
+                    number = number % 100;
+                }
+                else {
+                    result = subDigit('C', 'D', result);
+                    number = number % 100;
+                }
+            }
+            else if (number >= 50) {
+                if (number < 90) {
+                    result = digit('L', number / 50, result);
+                    number = number % 50;
+                }
+                else {
+                    result = subDigit('X', 'C', result);
+                    number = number % 10;
+                }
+            }
+            else if (number >= 10) {
                 if (number < 40) {
-                    i = digit('X', number / 10, i, c);
+                    result = digit('X', number / 10, result);
                     number = number % 10;
                 }
                 else {
-                    i = subDigit('X', 'L', i, c);
+                    result = subDigit('X', 'L', result);
                     number = number % 10;
                 }
             }
             else if (number >= 5) {
                 if (number < 9) {
-                    i = digit('V', number / 5, i, c);
+                    result = digit('V', number / 5, result);
                     number = number % 5;
                 }
                 else {
-                    i = subDigit('I', 'X', i, c);
+                    result = subDigit('I', 'X', result);
                     number = 0;
                 }
             }
             else if (number >= 1) {
                 if (number < 4) {
-                    i = digit('I', number, i, c);
+                    result = digit('I', number, result);
                     number = 0;
                 }
                 else {
-                    i = subDigit('I', 'V', i, c);
+                    result = subDigit('I', 'V', result);
                     number = 0;
                 }
             }
         }
 
-        return new String(c);
+        return result;
     }
 
     private static int value(char r) {
@@ -80,16 +107,18 @@ public class RomanDigit{
         }
     }
 
-    private static int subDigit(char num1, char num2, int i, char[] c) {
-        c[i++] = num1;
-        c[i++] = num2;
-        return i;
+    private static String subDigit(char num1, char num2, String result) {
+        result += num1;
+        result += num2;
+
+        return result;
     }
 
-    private static int digit(char ch, int n, int i, char[] c) {
+    private static String digit(char ch, int n, String result) {
         for (int j = 0; j < n; j++) {
-            c[i++] = ch;
+            result += ch;
         }
-        return i;
+
+        return result;
     }
 }
